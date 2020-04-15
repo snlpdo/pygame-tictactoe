a3949e58ff0f26c620b138d34829744e47fbbae7
Raphael Grosbois <Raphael.Grosbois@numericable.fr>
2020-04-15 19:15:08 +0200

    Exemple de déplacement d'une pièce à l'aide de la souris

--- engine.py
+++ engine.py
@@ -12,49 +12,21 @@ class Jeu:
     # 1: croix
     # 2: cercle
     self.plateau = [
-      [0,0,0],
+      [1,2,0],
       [0,0,0],
       [0,0,0],
     ]
 
-    # 2 joueurs 1 (croix) et 2 (cercle)
-    self.joueur = 1
-    self.fin = (False, 0)
 
-  def maj(self, c, l):
-    """
-    Prendre en compte un coup sur la cellule
-    de coordonnées c (colonne) et l (ligne)
-    """
-    # Vérifier que la case est libre
-    if self.plateau[l][c]!=0:
-      return
-    else:
-      self.plateau[l][c] = self.joueur
+  def occupant(self,colonne,ligne):
+    if self.plateau[ligne][colonne]==0: # case vide
+      return None
+    else: 
+      return self.plateau[ligne][colonne]
 
-    # Victoire ?
-    self.fin = self.victoire()
-
-    # Passer au joueur suivant
-    self.joueur= 2 - (self.joueur-1)
-
-  def victoire(self):
-        """
-        Vérifier si un joueur a gagné
-        """
-        
-        # variable locale (plus rapide)
-        p  = self.plateau 
-        
-        # Vérification des lignes et colonnes
-        for i in range(3):
-            if p[i][0]!=0 and p[i][0]==p[i][1]==p[i][2]:
-                return (True, p[i][0])
-            if p[0][i]!=0 and p[0][i]==p[1][i]==p[2][i]:
-                return (True, p[0][i])
-        # Vérifier les diagonales
-        if p[0][0]!=0 and p[0][0]==p[1][1]==p[2][2]:
-            return (True, p[0][0])
-        if p[2][0]!=0 and p[2][0]==p[1][1]==p[0][2]:
-            return (True, p[2][0])
-        return (False, 0)
+  def deplacer(self, dep, arr):
+    if self.plateau[arr[1]][arr[0]]==0: # arrivée libre
+        # recopier le type de pièce
+        self.plateau[arr[1]][arr[0]] = self.plateau[dep[1]][dep[0]]
+        # vider la case de départ
+        self.plateau[dep[1]][dep[0]] = 0
--- gui.py
+++ gui.py
@@ -46,19 +46,46 @@ pygame.draw.circle(cercle, NOIR, (int(w//2), int(h//2)), int(3*w//8), 3)
 # Horloge pour contrôler le fps
 clock = pygame.time.Clock()
 
+# Gestion du déplacement
+case_dep = ()
+position = ()
+piece_deplacee = None
+
 # Boucle principale
 continuer = True
 while continuer:
   # Gestion des évènements
   # (comme la fermeture de la fenêtre)
   for e in pygame.event.get():
+
     if e.type == pygame.QUIT:
       continuer = False
-    elif e.type == pygame.MOUSEBUTTONUP and not jeu.fin[0]:
-      position = e.pos
-      colonne = int(position[0]//(WIDTH/3))
-      ligne = int(position[1]//(HEIGHT/3))
-      jeu.maj(colonne, ligne)
+
+    elif e.type == pygame.MOUSEBUTTONDOWN: # Début
+        # colonne et ligne de départ
+        position = e.pos
+        colonne = int(position[0]//(WIDTH/3))
+        ligne = int(position[1]//(HEIGHT/3))
+        
+        # Vérifier s'il y a une pièce
+        piece_deplacee = jeu.occupant(colonne, ligne)
+        if piece_deplacee != None: # pièce sélectionnée
+            deplacement = True 
+            case_dep = (colonne, ligne)
+
+    elif e.type == pygame.MOUSEMOTION: # suivre le déplacement
+        if piece_deplacee != None:
+            position = e.pos
+
+    elif e.type == pygame.MOUSEBUTTONUP: # Fin
+        if piece_deplacee != None:
+            position = e.pos
+            colonne = int(position[0]//(WIDTH/3))
+            ligne = int(position[1]//(HEIGHT/3))
+            
+            case_arrivee = (colonne, ligne)
+            jeu.deplacer(case_dep, case_arrivee)
+            piece_deplacee = None
 
   ##############################
   # Mises à jour des propriétés
@@ -70,15 +97,21 @@ while continuer:
   screen.blit(bg, (0,0))
   for i in range(len(jeu.plateau)):
     for j in range(len(jeu.plateau[i])):
-      if jeu.plateau[i][j]==1: # une croix
-        screen.blit(croix, (j*WIDTH/3+2, i*HEIGHT/3+2))
-      elif jeu.plateau[i][j]==2: # un cercle
-        screen.blit(cercle, (j*WIDTH/3+2, i*HEIGHT/3+2))
-  
-  # En cas de victoire
-  if jeu.fin[0]:
-    pygame.display.set_caption("Victoire "+str(jeu.fin[1]))
+      # Ne pas dessiner la pièce qui se déplace
+      if piece_deplacee!=None and (j,i)==case_dep:
+          continue 
 
+      if jeu.plateau[i][j]==1: # une croix
+          screen.blit(croix, (j*WIDTH/3+2, i*HEIGHT/3+2))
+      elif jeu.plateau[i][j]==2: # un cercle
+          screen.blit(cercle, (j*WIDTH/3+2, i*HEIGHT/3+2))
+
+  # Dessin de la pièce qui bouge
+  if piece_deplacee == 1: # une croix
+      screen.blit(croix, position)
+  elif piece_deplacee == 2: # un cercle
+      screen.blit(cercle, position)
+  
   #########################  
   # raffraichir l'affichage
   pygame.display.flip()
