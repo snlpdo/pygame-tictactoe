7f5dd63a629cfc5bde9c42107cbb71b18694b177
Raphael Grosbois <Raphael.Grosbois@numericable.fr>
2020-05-11 13:12:31 +0200

    Début de validation de déplacement d'un pion blanc

--- engine.py
+++ engine.py
@@ -5,6 +5,23 @@ graphique.
 """
 
 
+def pion_blanc(depart, arrivee):
+    # Récupérer les indices de lignes et colonnes
+    col_dep, ligne_dep = depart
+    col_arr, ligne_arr = arrivee
+
+    # Règle 1: case d'arrivée sur la ligne supérieure
+    # ou 2 cases pour le premier déplacement
+    if not ((ligne_arr == ligne_dep - 2 and ligne_dep == 6) or ligne_arr == ligne_dep - 1):
+        return False
+
+    # Règle 2: cases dans la même colonne
+    if not (col_arr == col_dep):
+        return False
+
+    return True
+
+
 class Jeu:
 
     def __init__(self):
@@ -30,7 +47,17 @@ class Jeu:
             return self.plateau[ligne][colonne]
 
     def deplacer(self, dep, arr):
-        if self.plateau[arr[1]][arr[0]] == '':  # arrivée libre
+        # 1. Identifier la pièce à déplacer (=celle sur la case de départ)
+        # dep est un couple au format: (, )
+        colonne, ligne = dep
+        piece = self.plateau[ligne][colonne]
+
+        # 2. Vérifier la validité du déplacement
+        deplacement_valide = True  # valeur par défaut
+        if piece == 'pb':  # Pion blanc
+            deplacement_valide = pion_blanc(dep, arr)
+
+        if deplacement_valide:
             # recopier le type de pièce
             self.plateau[arr[1]][arr[0]] = self.plateau[dep[1]][dep[0]]
             # vider la case de départ
