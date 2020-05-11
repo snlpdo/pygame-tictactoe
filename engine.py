ea35d86130db91c685a2e98e09bb6326e2c3e813
Raphael Grosbois <Raphael.Grosbois@numericable.fr>
2020-05-11 12:16:16 +0200

    Transformation simple en un échiquier

--- engine.py
+++ engine.py
@@ -4,29 +4,34 @@ Il ne doit surtout pas supposer qu'il existe une interface
 graphique.
 """
 
+
 class Jeu:
 
-  def __init__(self):
-    # Contenu des cellules:
-    # 0: case vide
-    # 1: croix
-    # 2: cercle
-    self.plateau = [
-      [1,2,0],
-      [0,0,0],
-      [0,0,0],
-    ]
+    def __init__(self):
+        # Contenu des cellules:
+        # 0: case vide
+        # 1: croix
+        # 2: cercle
+        self.plateau = [
+            ['tn', 'cn', 'fn', 'dn', 'rn', 'fn', 'cn', 'tn'],
+            ['pn', 'pn', 'pn', 'pn', 'pn', 'pn', 'pn', 'pn'],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['pb', 'pb', 'pb', 'pb', 'pb', 'pb', 'pb', 'pb'],
+            ['tb', 'cb', 'fb', 'db', 'rb', 'fb', 'cb', 'tb']
+        ]
 
+    def occupant(self, colonne, ligne):
+        if self.plateau[ligne][colonne] == '':  # case vide
+            return None
+        else:
+            return self.plateau[ligne][colonne]
 
-  def occupant(self,colonne,ligne):
-    if self.plateau[ligne][colonne]==0: # case vide
-      return None
-    else: 
-      return self.plateau[ligne][colonne]
-
-  def deplacer(self, dep, arr):
-    if self.plateau[arr[1]][arr[0]]==0: # arrivée libre
-        # recopier le type de pièce
-        self.plateau[arr[1]][arr[0]] = self.plateau[dep[1]][dep[0]]
-        # vider la case de départ
-        self.plateau[dep[1]][dep[0]] = 0
+    def deplacer(self, dep, arr):
+        if self.plateau[arr[1]][arr[0]] == '':  # arrivée libre
+            # recopier le type de pièce
+            self.plateau[arr[1]][arr[0]] = self.plateau[dep[1]][dep[0]]
+            # vider la case de départ
+            self.plateau[dep[1]][dep[0]] = ''
--- gui.py
+++ gui.py
@@ -1,4 +1,6 @@
 import pygame
+import pygame.freetype
+import os
 from engine import Jeu
 
 """
@@ -7,10 +9,13 @@ et de l'interaction avec l'utilisateur (gestion de la souris et/ou du clavier)
 """
 
 # Constantes utiles
-WIDTH = 150
-HEIGHT = 150
-BLANC = (255,255,255)
-NOIR = (0,0,0)
+CZ = 50
+WIDTH, HEIGHT = CZ * 10, CZ * 10
+BGCOLOR = (213, 166, 49)
+BLANC = (255, 255, 255)
+MARRON = (97, 82, 74)
+NOIR = (0, 0, 0)
+ROUGE = (255, 0, 0)
 
 # Démarrer la bibliothèque
 pygame.init()
@@ -19,28 +24,86 @@ pygame.init()
 jeu = Jeu()
 
 # Définir la taille de la fenêtre en pixels
-screen = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("Tic Tac Toe")
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Échecs")
 
 ##################################################
-# Préparer les images d'arrière plan et les pièces
-bg = pygame.Surface((WIDTH,HEIGHT))
-bg.fill(BLANC)
-pygame.draw.line(bg, NOIR, (0,HEIGHT/3), (WIDTH,HEIGHT/3), 3)
-pygame.draw.line(bg, NOIR, (0,2*HEIGHT/3), (WIDTH,2*HEIGHT/3), 3)
-pygame.draw.line(bg, NOIR, (WIDTH/3,0), (WIDTH/3,HEIGHT), 3)
-pygame.draw.line(bg, NOIR, (2*WIDTH/3,0), (2*WIDTH/3,HEIGHT), 3)
+# Préparer l'arrière plan
+police = pygame.freetype.SysFont('overpass', 30, True, False)
+police_petite = pygame.freetype.SysFont('overpass', 20, True, False)
+police_mini = pygame.freetype.SysFont('overpass', 14, True, False)
 
-w = WIDTH/3-4
-h = HEIGHT/3-4
-croix = pygame.Surface((w,h))
-croix.fill(BLANC)
-pygame.draw.line(croix, NOIR, (w/4, h/4), (3*w/4, 3*h/4), 3)
-pygame.draw.line(croix, NOIR, (w/4, 3*h/4), (3*w/4, h/4), 3)
+background = pygame.Surface((WIDTH, HEIGHT))
+background.fill(BGCOLOR)
+toggle = True
+for i in range(8):
+    text, r = police.render(str(8 - i), NOIR)
+    background.blit(text, (CZ / 2 - r.width / 2, (i + 1) * CZ + CZ / 2 - r.height / 2))
 
-cercle = pygame.Surface((w,h))
-cercle.fill(BLANC)
-pygame.draw.circle(cercle, NOIR, (int(w//2), int(h//2)), int(3*w//8), 3)
+    for j in range(8):
+        if i == 0:
+            text, r = police.render(chr(j + ord('A')), NOIR)
+            background.blit(text, ((j + 1) * CZ + CZ / 2 - r.width / 2, CZ / 2 - r.height / 2))
+
+        if toggle:
+            pygame.draw.rect(background, BLANC, ((j + 1) * CZ, (i + 1) * CZ, CZ, CZ), 0)
+        else:
+            pygame.draw.rect(background, MARRON, ((j + 1) * CZ, (i + 1) * CZ, CZ, CZ), 0)
+        toggle = not (toggle)
+    toggle = not (toggle)
+
+background.convert()
+
+
+##################################################
+# Préparer les pièces
+
+
+def charger(filename):
+    img = pygame.image.load(os.path.join(curdir, 'img', filename)).convert_alpha()
+    return pygame.transform.scale(img, (CZ, CZ))
+
+
+def get_image(piece):
+    if piece == 'tn':
+        return image_tn
+    elif piece == 'cn':
+        return image_cn
+    elif piece == 'fn':
+        return image_fn
+    elif piece == 'dn':
+        return image_dn
+    elif piece == 'rn':
+        return image_rn
+    elif piece == 'pn':
+        return image_pn
+    elif piece == 'pb':
+        return image_pb
+    elif piece == 'rb':
+        return image_rb
+    elif piece == 'db':
+        return image_db
+    elif piece == 'fb':
+        return image_fb
+    elif piece == 'cb':
+        return image_cb
+    elif piece == 'tb':
+        return image_tb
+
+
+curdir = os.getcwd()
+image_tn = charger('tour_noire.png')
+image_cn = charger('cavalier_noir.png')
+image_dn = charger('dame_noire.png')
+image_rn = charger('roi_noir.png')
+image_fn = charger('fou_noir.png')
+image_pn = charger('pion_noir.png')
+image_tb = charger('tour_blanche.png')
+image_cb = charger('cavalier_blanc.png')
+image_db = charger('dame_blanche.png')
+image_rb = charger('roi_blanc.png')
+image_fb = charger('fou_blanc.png')
+image_pb = charger('pion_blanc.png')
 ####################################################
 
 # Horloge pour contrôler le fps
@@ -54,71 +117,75 @@ piece_deplacee = None
 # Boucle principale
 continuer = True
 while continuer:
-  # Gestion des évènements
-  # (comme la fermeture de la fenêtre)
-  for e in pygame.event.get():
+    # Gestion des évènements
+    # (comme la fermeture de la fenêtre)
+    for e in pygame.event.get():
 
-    if e.type == pygame.QUIT:
-      continuer = False
+        if e.type == pygame.QUIT:
+            continuer = False
 
-    elif e.type == pygame.MOUSEBUTTONDOWN: # Début
-        # colonne et ligne de départ
-        position = e.pos
-        colonne = int(position[0]//(WIDTH/3))
-        ligne = int(position[1]//(HEIGHT/3))
-        delta = ( position[0]%int(WIDTH/3), position[1]%int(HEIGHT/3) )
-        
-        # Vérifier s'il y a une pièce
-        piece_deplacee = jeu.occupant(colonne, ligne)
-        if piece_deplacee != None: # pièce sélectionnée
-            deplacement = True 
-            case_dep = (colonne, ligne)
-
-    elif e.type == pygame.MOUSEMOTION: # suivre le déplacement
-        if piece_deplacee != None:
+        elif e.type == pygame.MOUSEBUTTONDOWN:  # Début
+            # colonne et ligne de départ
             position = e.pos
+            colonne = int(position[0] // CZ) - 1
+            ligne = int(position[1] // CZ) - 1
+            delta = (position[0] % CZ, position[1] % CZ)
+            if not (0 <= ligne <= 7 and 0 <= colonne <= 7):
+                continue
 
-    elif e.type == pygame.MOUSEBUTTONUP: # Fin
-        if piece_deplacee != None:
-            position = e.pos
-            colonne = int(position[0]//(WIDTH/3))
-            ligne = int(position[1]//(HEIGHT/3))
-            
-            case_arrivee = (colonne, ligne)
-            jeu.deplacer(case_dep, case_arrivee)
-            piece_deplacee = None
+            # Vérifier s'il y a une pièce
+            piece_deplacee = jeu.occupant(colonne, ligne)
+            if piece_deplacee:  # pièce sélectionnée
+                deplacement = True
+                case_dep = (colonne, ligne)
 
-  ##############################
-  # Mises à jour des propriétés
-  # du contenu
-  
-  
-  #########################
-  # Dessin du contenu
-  screen.blit(bg, (0,0))
-  for i in range(len(jeu.plateau)):
-    for j in range(len(jeu.plateau[i])):
-      # Ne pas dessiner la pièce qui se déplace
-      if piece_deplacee!=None and (j,i)==case_dep:
-          continue 
+        elif e.type == pygame.MOUSEMOTION:  # suivre le déplacement
+            if piece_deplacee:
+                position = e.pos
 
-      if jeu.plateau[i][j]==1: # une croix
-          screen.blit(croix, (j*WIDTH/3+2, i*HEIGHT/3+2))
-      elif jeu.plateau[i][j]==2: # un cercle
-          screen.blit(cercle, (j*WIDTH/3+2, i*HEIGHT/3+2))
+        elif e.type == pygame.MOUSEBUTTONUP:  # Fin
+            if piece_deplacee:
+                position = e.pos
+                colonne = int(position[0] // CZ) - 1
+                ligne = int(position[1] // CZ) - 1
+                if not (0 <= ligne <= 7 and 0 <= colonne <= 7):
+                    continue
 
-  # Dessin de la pièce qui bouge
-  if piece_deplacee == 1: # une croix
-      screen.blit(croix, (position[0]-delta[0], position[1]-delta[1]) )
-  elif piece_deplacee == 2: # un cercle
-      screen.blit(cercle, (position[0]-delta[0], position[1]-delta[1]) )
-  
-  #########################  
-  # raffraichir l'affichage
-  pygame.display.flip()
+                case_arrivee = (colonne, ligne)
+                jeu.deplacer(case_dep, case_arrivee)
+                piece_deplacee = None
 
-  # fps: ici 30 image par seconde
-  clock.tick(30)
+    ##############################
+    # Mises à jour des propriétés
+    # du contenu
+
+    #########################
+    # Arrière-plan
+    screen.blit(background, (0, 0))
+
+    # Pièces statiques sur l'échiquier
+    for i in range(len(jeu.plateau)):
+        for j in range(len(jeu.plateau[i])):
+            # ne pas dessiner la pièce déplacée
+            if piece_deplacee and case_dep == (j, i):
+                continue
+
+            image = get_image(jeu.plateau[i][j])
+            if image:
+                screen.blit(image, ((j + 1) * CZ, (i + 1) * CZ))
+
+    # Dessiner la pièce qui bouge
+    if piece_deplacee:
+        colonne, ligne = case_dep
+        image = get_image(jeu.plateau[ligne][colonne])
+        screen.blit(image, (position[0] - delta[0], position[1] - delta[1]))
+
+    #########################
+    # raffraichir l'affichage
+    pygame.display.flip()
+
+    # fps: ici 30 image par seconde
+    clock.tick(30)
 
 # Terminer l'application
 pygame.quit()
